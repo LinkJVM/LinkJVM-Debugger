41e0c7820d68b39a4b4b74dad62e5bb2c02ec07a
kleiinnn <m@mklein.co.at>
2014-01-08 21:49:58 +0100

    implemented server part

--- src/linkjvm/debugger/client/Debugger.java
+++ src/linkjvm/debugger/client/Debugger.java
@@ -4,12 +4,5 @@ public class Debugger {
 	public static void main(String[] args){
 		Server server = new Server();
 		server.start();
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		server.stop();
 	}
 }
--- src/linkjvm/debugger/client/Server.java
+++ src/linkjvm/debugger/client/Server.java
@@ -1,6 +1,95 @@
 package linkjvm.debugger.client;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 
 public class Server{
+	public static final int PORT = 62512;
 	
+	private ServerSocket serverSocket;
+	private final LinkedList<Socket> clients;
+	
+	private boolean stopAcceptThread = false;
+	private boolean stopListenThread = false;
+	
+	public Server(){
+		try {
+			serverSocket = new ServerSocket(PORT);
+			System.out.println("[DEBUG]: Server started on port " + PORT);
+		} catch (IOException e) {
+			System.out.println("[ERROR]: Cannot create server on port " + PORT + ". Use another port.");
+			e.printStackTrace();
+		}
+		clients = new LinkedList<Socket>();
+	}
+	
+	public void start(){
+		new Thread(new AcceptThread()).start();
+		new Thread(new ListenThread()).start();
+	}
+	
+	public void stop(){
+		System.out.print("[DEBUG]: Closing down...");
+		stopAcceptThread = true;
+		stopListenThread = true;
+		try {
+			serverSocket.close();
+		} catch (IOException e) { }
+		if(stopListenThread){
+			for(Socket client : clients){
+				try {
+					client.close();
+				} catch (IOException e) {
+					System.out.println("[ERROR]: Could not close connect to client " + client + ".");
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("Done.");
+	}
+	
+	private class AcceptThread implements Runnable {
+
+		@Override
+		public void run() {
+			while(!stopAcceptThread){
+				Socket client = null;
+				try {
+					client = serverSocket.accept();
+					synchronized(clients){
+						clients.add(client);
+					}
+				} catch (IOException e) {	}
+			}
+		}
+		
+	}
+	
+	
+	private class ListenThread implements Runnable {
+
+		@Override
+		public void run() {
+			while(!stopListenThread){
+				synchronized(clients){
+					for(Socket client : clients){
+						try {
+							Scanner input = new Scanner(client.getInputStream());
+							if(input.hasNextLine()){
+								System.out.println(input.nextLine());
+							}
+							input.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+	}
 }
